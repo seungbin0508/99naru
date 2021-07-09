9f804c1247f7e204d9c74e066729793da7a58b10
ruzann28 <ruzann@daum.net>
2021-07-09 21:11:59 +0900

    회원가입, 중복검사, 로그인 api 경로 추가

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,6 +9,8 @@ const router = express.Router();
 
 router.use("/posts", postsRouter);
 router.use("/comments", commentsRouter);
+
+// 회원가입 중복검사 로그인 경로
 router.use("/signup", signupRouter);
 router.use("/duplicate", duplicateRouter);
 router.use("/sigin", signinRouter);
