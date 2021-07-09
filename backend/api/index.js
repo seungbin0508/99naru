8f2a86f0f97f8561c59c28677427a76a2b92137e
soujinko <soujinko@naver.com>
2021-07-09 16:51:14 +0900

    signup 라우터 추가

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import postsRouter from './posts.js'
-import commentsRouter from './comments.js'
+import commentsRouter from './comments'
+import usersRouter from './users'
+
 
 const router = express.Router()
 
 router.use('/posts', postsRouter)
 router.use('/comments', commentsRouter)
+router.use('/signup', usersRouter)
 
 export default router
