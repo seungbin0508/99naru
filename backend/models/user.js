90c25f7e523db079e2590b964b35669fcc9d405f
ruzann28 <ruzann@daum.net>
2021-07-09 21:05:48 +0900

    회원가입 db 스키마

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    loginId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nickname: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+});
+
+module.exports = mongoose.model('User', userSchema);
